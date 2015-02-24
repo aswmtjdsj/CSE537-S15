54db9a8aef819d1bbe345132fc9420e81bbb8d62
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-02-24 12:41:35 -0500

    IDS method done; false test data added

--- proj-1/solution.py
+++ proj-1/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys, os
+import copy
 from solution_ids import IDS
 from solution_astar import *
 
@@ -11,6 +12,10 @@ mapping = {} # data mapping from string to board status
 mapping['-'] = -1 # for unusable grid
 mapping['O'] = 0 # for empty grid
 mapping['X'] = 1 # for peg grid
+back_mapping = {}
+back_mapping[-1] = '-'
+back_mapping[0] = 'O'
+back_mapping[1] = 'X'
 
 original_board = [] # hold the input string arrays
 board = [] # supposed to be 7 * 7 array
@@ -49,16 +54,65 @@ if __name__ == '__main__':
         print i
     print ''
 
+    index_mapping_cnt = 0
+    index_mapping = copy.deepcopy(board)
+    for idx, row in enumerate(board):
+        for jdx, grid in enumerate(row): # use for (x, y) to (mapping_id) tranformation
+            if grid != -1:
+                index_mapping[idx][jdx] = index_mapping_cnt
+                index_mapping_cnt += 1
+
+    print 'Index Mapping'
+    for i in index_mapping:
+        print i
+    print ''
+
     try:
+        result = None
         if sys.argv[2] == '--IDS':
-            IDS(board)
+            result = IDS(board)
         elif sys.argv[2] == '--ASTAR':
-            ASTAR(board)
+            result = ASTAR(board)
+        
+        if result != None:
+            result.reverse()
+            show_result = map(lambda x: (index_mapping[x[0][0]][x[0][1]], index_mapping[x[1][0]][x[1][1]]), result)
+            print 'Solution Found >>>>'
+            print result
+            print show_result
+            print ''
+
+            print 'Solution moves >>>>'
+            print ''
+
+            move_cnt = 0
+            print move_cnt
+            move_board = copy.deepcopy(board)
+            for i in original_board: print i
+            print ''
+
+            for i in result:
+                move_cnt += 1
+                print move_cnt
+                from_peg = i[0]
+                to_grid = i[1]
+                next_peg = ((i[0][0]+i[1][0])/2, (i[0][1]+i[1][1])/2)
+                # print from_peg
+                # print next_peg
+                # print to_grid
+                move_board[from_peg[0]][from_peg[1]] = 0
+                move_board[next_peg[0]][next_peg[1]] = 0
+                move_board[to_grid[0]][to_grid[1]] = 1
+                draw_board = map(lambda x: ''.join(map(lambda y: back_mapping[y], x)), move_board)
+                for j in draw_board:
+                    print j
+                print ''
+
     except Exception as e:
         # print dir(e)
-        if e.__class__ == NameError:
-            raise Exception('ASTAR() or IDS() method hasn\'t been implemented')
-        elif e.__class__ == IndexError:
+        # if e.__class__ == NameError:
+        #     raise Exception('ASTAR() or IDS() method hasn\'t been implemented')
+        if e.__class__ == IndexError:
             raise Exception('''command should be "python solution.py <data_file> <strategy>"
             You Should Choose Strategy from "--IDS" or "--ASTAR"''')
         else:
--- proj-1/solution_ids.py
+++ proj-1/solution_ids.py
@@ -1,21 +1,55 @@
 import copy
 
-def DLS_work(pegs, target, boundary, limit, depth):
+def DLS_work(pegs, cur_board, target, boundary, limit, depth):
     over_limit = False
 
-    if len(pegs) == 1 and pegs == target:
+    if len(pegs) == 1 and pegs[0] == target: # Goal arrived
         return True
-
-    if depth == limit:
-        return -1 # due to the limit of depth
+    if depth == limit: # depth limit reached
+        return -1
 
     # expand
-    # TODO
-    direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    in_board = lambda pos: pos[0] >= 0 and pos[0] < len(boundary) and pos[1] >= 0 and pos[1] < len(boundary[0]) and boundary[pos[0]][pos[1]] != -1
-    # print in_board((3, 3))
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)] # four directions
+    in_board = lambda pos: pos[0] >= 0 and pos[0] < len(boundary) and pos[1] >= 0 and pos[1] < len(boundary[0]) and boundary[pos[0]][pos[1]] != -1 # peg position legality checking
+    move = lambda x, y: (x[0]+y[0], x[1]+y[1]) # peg move function
+    for peg in pegs:
+        for d in directions:
+            next_peg = move(peg, d)
+            if in_board(next_peg) and cur_board[next_peg[0]][next_peg[1]] == 1: 
+                # there's a peg in the next position
+                jump_grid = move(next_peg, d)
+                if in_board(jump_grid) and cur_board[jump_grid[0]][jump_grid[1]] == 0:
+                    # there's an empty grid in the next to next
+                    cur_move = (peg, jump_grid)
+                    next_board = copy.deepcopy(cur_board)
+                    next_board[peg[0]][peg[1]] = 0
+                    next_board[next_peg[0]][next_peg[1]] = 0
+                    next_board[jump_grid[0]][jump_grid[1]] = 1
+                    new_pegs = copy.deepcopy(pegs)
+                    new_pegs.remove(peg)
+                    new_pegs.remove(next_peg)
+                    new_pegs.append(jump_grid)
+                    result = DLS_work(new_pegs, next_board, target, boundary, limit, depth+1)
+                    if result == -1:
+                        over_limit = True
+                    elif result != False:
+                        if result == True:
+                            return [cur_move]
+                        else:
+                            result.append(cur_move)
+                            return result
+
+    if over_limit == True:
+        return -1
+    else:
+        return False
 
 def DLS(board, limit):
+    '''
+    depth limited search
+    '''
+    print "Current Depth Limit: {0}".format(limit)
+    print ''
     pegs = []
     boundary = copy.deepcopy(board)
 
@@ -30,33 +64,29 @@ def DLS(board, limit):
     transform[-1] = -1
 
     boundary = map(lambda x: map(lambda y: transform[y], x), boundary)
-    print 'Boundary of Board >>>>'
-    for i in boundary:
-        print i
-    print ''
+    # print 'Boundary of Board >>>>'
+    # for i in boundary:
+    #     print i
+    # print ''
 
-    print 'Original Pegs >>>>'
-    for i in pegs:
-        print i
-    print ''
-
-    mapping_cnt = 0
-    mapping = copy.deepcopy(board)
-    for idx, row in enumerate(board):
-        for jdx, grid in enumerate(row): # use for (x, y) to (mapping_id) tranformation
-            if grid != -1:
-                mapping[idx][jdx] = mapping_cnt
-                mapping_cnt += 1
-
-    print 'Mapping'
-    for i in mapping:
-        print i
-    print ''
+    # print 'Original Pegs >>>>'
+    # for i in pegs:
+    #     print i
+    # print ''
 
     tar_peg = (len(board)/2, len(board[0])/2)
-    if DLS_work(pegs, tar_peg, boundary, limit, 0) == True:
-        return True
-    return False
+    result = DLS_work(pegs, board, tar_peg, boundary, limit, 0)
+
+    if result == -1:
+        print "Depth Limit {0} reached".format(limit)
+        print ''
+        return result
+    elif result == False:
+        print 'DLS didn\'t find any solutions'
+        print ''
+        return False
+    else:
+        return result
 
 def IDS(board):
     '''
@@ -67,9 +97,13 @@ def IDS(board):
 
     limit = 0
     while True:
-        if DLS(board, limit):
-            return True
+        result = DLS(board, limit)
+        if result == False:
+            return None
+        elif result != -1:
+            return result
         limit += 1
 
-        if limit > 10: # in case of infinite loop, but actually no such case
-            break
+        # if limit > 10: # in case of infinite loop, but actually no such case
+        #     break
+    return None
