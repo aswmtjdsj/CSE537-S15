28d9a7ab8659975050afb37e7b8c5ac15a5a3baf
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-03-22 19:52:42 -0400

    add simple test data

--- proj-3/handle.py
+++ proj-3/handle.py
@@ -42,35 +42,37 @@ course_mapping = {}
 TA_mapping = {}
 
 # common help variables
-command_help = 'To run this program, you should type {python} ./handle.py <--file> [file_name] <--method> [method_name]'
+command_help = 'To run this program, you should type {python} ./handle.py [file_name] {{<--method> [method_name]}}'
 method_list = ['BS', 'BS_FC', 'BS_FC_CP', 'ALL']
 data_part_info = ['Time each course is taking place', 'Course recitations', 'Course details', 'Course requirements', 'TA responsibilities', 'TA skills']
 rect_period = 90
+debug = True # whether to print auxillary information
 
 if __name__ == '__main__':
     """
+    main function
     """
+    print 'Parsing command options ...'.format(sys.argv)
     method_name = ''
     file_name = ''
     try:
         # parse command options
-        if len(sys.argv) < 3:
-            raise Exception('number of options < 5')
+        if len(sys.argv) < 2: # insufficient command options, at least lack of file name
+            raise Exception('number of options < 2, No file specified')
 
-        file_opt = sys.argv[1]
-        if file_opt != '--file':
-            raise Exception('No file specified')
+        file_name = sys.argv[1]
 
-        file_name = sys.argv[2]
-
-        if len(sys.argv) > 3:
-            method_opt = sys.argv[3]
+        if len(sys.argv) > 2:
+            method_opt = sys.argv[2]
             if method_opt != '--method':
-                raise Exception('No method specified')
-
-            method_name = sys.argv[4]
-            if method_name not in method_list:
-                raise Exception('No such method {0}'.format(method_name))
+                raise Exception('Unknown option: {0}'.format(sys.argv[2]))
+            else:
+                if len(sys.argv) < 4:
+                    raise Exception('No method specified')
+                else:
+                    method_name = sys.argv[3]
+                    if method_name not in method_list:
+                        raise Exception('No such method {0}'.format(method_name))
         else:
             method_opt = method_list[-1]
 
@@ -80,11 +82,15 @@ if __name__ == '__main__':
         exit()
     
     try:
+        # parse data file
         data_cnt = 0
         TA_list = []
+        print 'Parsing data file {0} ...'.format(file_name)
         with open(file_name) as data_file:
-            print '{0}. {1}:'.format(data_cnt + 1, data_part_info[data_cnt])
+            if debug:
+                print '{0}. {1}:'.format(data_cnt + 1, data_part_info[data_cnt])
             for line in data_file:
+                # handle new line to parse different parts of data file
                 if line == '\n':
                     data_cnt += 1
                     print '\n{0}. {1}:'.format(data_cnt + 1, data_part_info[data_cnt])
@@ -100,7 +106,8 @@ if __name__ == '__main__':
                     time = data[2::2]
                     course_item.time_list = zip(day, time)
                     course_mapping[course_item.course_id] = course_item
-                    print course_mapping[course_item.course_id]
+                    if debug:
+                        print course_mapping[course_item.course_id]
 
                 elif data_cnt == 1:
                     # handle course recitations 
@@ -108,7 +115,8 @@ if __name__ == '__main__':
                     day = data[1::2]
                     time = data[2::2]
                     course_item.rec_list = zip(day, time)
-                    print course_mapping[course_item.course_id]
+                    if debug:
+                        print course_mapping[course_item.course_id]
 
                 elif data_cnt == 2:
                     # handle course details
@@ -123,7 +131,8 @@ if __name__ == '__main__':
                         course_item.num_TA = 1.5
                     else:
                         course_item.num_TA = 2
-                    print course_mapping[course_item.course_id]
+                    if debug:
+                        print course_mapping[course_item.course_id]
 
                 elif data_cnt == 3:
                     # handle course requirements
@@ -140,14 +149,22 @@ if __name__ == '__main__':
                     time = data[2::2]
                     TA_item.taken_list = zip(day, time)
                     TA_mapping[TA_item.name] = TA_item
-                    print TA_mapping[TA_item.name]
+                    if debug:
+                        print TA_mapping[TA_item.name]
 
                 else:
                     # handle TA skill list 
                     TA_item = TA_mapping[data[0]]
                     skill_list = data[1:]
                     TA_item.skill_list = skill_list
-                    print TA_mapping[TA_item.name]
+                    if debug:
+                        print TA_mapping[TA_item.name]
 
+    except Exception as e:
+        raise e
+
+    try:
+        # build CSP
+        pass
     except Exception as e:
         raise e
