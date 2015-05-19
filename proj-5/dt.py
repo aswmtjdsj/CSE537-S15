bd5a42b28eddaf494bce961f7ee9f2ccfc262c09
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-05-19 18:24:35 -0400

    clickstream decision tree solution init

---
+++ proj-5/dt.py
@@ -0,0 +1,14 @@
+import os, sys
+
+class DecisionTree():
+    '''
+    Decision Tree Class
+    '''
+    def __init__(self):
+        pass
+    def __str__(self):
+        return 'tree'
+
+if __name__ == '__main__':
+    dt = DecisionTree()
+    print dt
