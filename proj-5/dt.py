823c02e32e84829c12c8c0e8bf5f892f735d2f3f
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-05-20 00:19:29 -0400

    decision tree done, without early stopping

--- proj-5/dt.py
+++ proj-5/dt.py
@@ -1,4 +1,25 @@
 import os, sys
+import math
+
+NODE_TYPE = ['DS', 'LS', 'ES', 'MIG'] # all data same, all label same, early stopped, maximum information gain
+
+class TreeNode():
+    def __init__(self):
+        self.feature_id = None
+        self.children = []
+        self.end_node = False
+        self.node_type = None
+        self.split = [0, 0]
+        self.label = None
+        self.value = None
+
+    def __str__(self):
+        return str({'feature_id': self.feature_id,
+                'children': self.children,
+                'node_type': self.node_type,
+                'split': self.split,
+                'label': self.label,
+                'value': self.value})
 
 class DecisionTree():
     '''
@@ -7,11 +28,10 @@ class DecisionTree():
     def __init__(self):
         self.train_data = []
 
-        self.tree = None
         self.size = None
 
         self.dimension = None
-        self.feature_sets = []
+        self.root = None
 
         self.debug = True
 
@@ -25,12 +45,6 @@ class DecisionTree():
         for i in range(self.size):
             if self.dimension == None: # init dimensionality
                 self.dimension = len(data[i])
-                self.feature_sets = [None] * self.dimension
-            for j in range(self.dimension): 
-                if self.feature_sets[j] == None:
-                    self.feature_sets[j] = set([data[i][j]])
-                elif data[i][j] not in self.feature_sets[j]:
-                    self.feature_sets[j].add(data[i][j])
             if len(data[i]) != self.dimension: # check missing features
                 raise Exception('data dimension not match: {0} (dimension) and {1} (new data item dimension)'.format(self.dimension, len(data[i])))
             self.train_data.append((data[i], label[i]))
@@ -38,11 +52,137 @@ class DecisionTree():
         f_l.close()
 
     def build(self):
-        pass
+        self.root = self._build(self.train_data)
+
+    def _build(self, train_data):
+
+        data = map(lambda x: x[0], train_data)
+        label = map(lambda x: x[1], train_data)
+
+        node_end = False 
+        node_type = None
+        node = TreeNode()
+
+        label_set = set(label)
+        node.split[0] = len(filter(lambda x: x == bool(0), label))
+        node.split[1] = len(filter(lambda x: x == bool(1), label))
+
+        if len(label_set) == 1: # all labels are the same
+            node_end = True
+            node.node_type = NODE_TYPE[1]
+            node.label = list(label_set)[0]
+            node.end_node = True
+            return node
+
+        feature_count = None
+        for i in range(len(data)):
+            if feature_count == None: # init dimensionality
+                feature_count = [None] * self.dimension # dict for the number of data as for different features
+            for j in range(self.dimension): 
+                if feature_count[j] == None:
+                    feature_count[j] = {data[i][j]: [0, 0]}
+                elif data[i][j] not in feature_count[j]:
+                    feature_count[j][data[i][j]] = [0, 0]
+                feature_count[j][data[i][j]][int(label[i])] += 1
+
+        # print feature_count
+
+        all_feature_same = filter(lambda x: len(x) > 1, feature_count)
+        if len(all_feature_same) == 0:
+            node.end_node = True
+            node.node_type = NODE_TYPE[0]
+            return node
+
+        h_x = sum(map(lambda x: - (x * 1. / len(label)) * math.log(x * 1. / len(label)) if x != 0 else 0., node.split))
+        # print 'h_x: {0}'.format(h_x), [len(filter(lambda x: x == i, label)) for i in label_set]
+        # if node.split == [3, 3]:
+        #    print feature_count
+        #    print map(lambda x: len(x), feature_count)
+
+        # IG(X, Y) = H(X) - sigma( C(Y) / C(X) * H(Y))
+        ig_x_y = [None] * self.dimension
+        for i in range(self.dimension):
+            if len(feature_count[i]) == 1:
+                ig_x_y[i] = 0.
+                continue
+
+            h_y = 0
+            for j in feature_count[i].keys():
+                num_example = sum(feature_count[i][j])
+                h_x_y = 0
+                for l in label_set:
+                    if feature_count[i][j][l] > 0:
+                        p = feature_count[i][j][l] * 1. / num_example
+                        h_x_y += -p * math.log(p)
+                h_y += num_example * 1. / len(label) * h_x_y
+            ig_x_y[i] = h_x - h_y
+            # print feature_count[i], ig_x_y[i]
+        # print max(ig_x_y), ig_x_y.index(max(ig_x_y)), feature_count[ig_x_y.index(max(ig_x_y))]
+
+        if max(ig_x_y) < 1e-6: # feature impossible to be divided
+            node.end_node = True
+            node.node_type = NODE_TYPE[0]
+            return node
+
+        node.feature_id = ig_x_y.index(max(ig_x_y))
+        for j in feature_count[node.feature_id]:
+            sub_data = filter(lambda x: x[0][node.feature_id] == j, train_data)
+            # print 'sub_data: {0}'.format(sub_data)
+            node.children.append(self._build(sub_data))
+            node.children[-1].value = j
+
+        node.node_type = NODE_TYPE[3]
+        return node
+
+    def traverse(self):
+        queue = []
+        queue.append((self.root, 0))
+        cur = 0
+        pt = 0
+        while pt < len(queue):
+            head = queue[pt]
+            if head[1] != cur:
+                cur = head[1]
+                print '\n'
+            else:
+                print '({0}, ({1}, {2}), {3}, {4}, {5})'.format(head[0].feature_id, head[0].split[0], head[0].split[1], head[0].node_type, head[0].value, head[0].label),
+            pt += 1
+            if head[0].children != None:
+                for i in head[0].children:
+                    queue.append((i, head[1] + 1))
+        print '\n'
 
     def test(self, test_data):
-        if self.tree == None:
+        if self.root == None:
             raise Exception('No tree trained yet!')
+
+        result = None
+        cur_node = self.root
+        cnt = 0
+        while result == None:
+            cnt += 1
+            next_node = None
+            if cur_node.node_type == 'LS':
+                result = cur_node.label
+                break
+
+            if cur_node.node_type == 'DS':
+                break
+
+            if cur_node.children == None:
+                break
+
+            for i in cur_node.children:
+                if test_data[cur_node.feature_id] == i.value:
+                    next_node = i
+                    break
+                # if no hit, then missing value for one feature
+            if next_node != None:
+                cur_node = next_node
+            else: # missing value
+                break
+
+        return result
 
     def __str__(self):
         if self.size == None:
@@ -52,7 +192,48 @@ class DecisionTree():
             # if self.debug:
                 for i in self.train_data:
                     print "data {{{0}}}: {1}".format(i[0], i[1])
-            return 'tree size: {0}, dimension: {1}\nfeature set: {2}'.format(self.size, self.dimension, self.feature_sets)
+            # return 'tree size: {0}, dimension: {1}\nfeature count: {2}'.format(self.size, self.dimension, self.feature_count)
+            return 'tree size: {0}'.format(self.size)
+
+def unit_test(dt, data_set, data_label):
+    cnt = 0
+    t = 0
+    f = 0
+    n = 0
+    print len(data_set)
+    for i in range(len(data_set)):
+        r = dt.test(data_set[i])
+        if r == True:
+            t += 1
+        elif r == False:
+            f += 1
+        else:
+            n += 1
+        if r == data_label[i]:
+            cnt += 1
+    print ''
+    print t, f, n
+    print [len(filter(lambda x: x == i, data_label)) for i in set(data_label)]
+    print cnt, dt.size, cnt * 1. / len(data_set) 
+
+def load(f_d, f_l):
+    f_d = open(f_d, 'r')
+    f_l = open(f_l, 'r')
+    data = [map(int, i) for i in map(lambda x: x.strip().split(' '), f_d.read().strip().split('\n'))] # turn data line into feature list
+    label = map(bool, map(int, f_l.read().strip().split())) # turn label line into bool label
+    # print len(data), len(label)
+    size = len(data)
+    dimension = None
+    test_data = []
+    for i in range(size):
+        if dimension == None: # init dimensionality
+            dimension = len(data[i])
+        if len(data[i]) != dimension: # check missing features
+            raise Exception('data dimension not match: {0} (dimension) and {1} (new data item dimension)'.format(self.dimension, len(data[i])))
+        test_data.append((data[i], label[i]))
+    f_d.close()
+    f_l.close()
+    return test_data
 
 if __name__ == '__main__':
     f_train_data = 'clickstream/trainfeat.csv'
@@ -62,4 +243,15 @@ if __name__ == '__main__':
 
     dt = DecisionTree()
     dt.load(f_train_data, f_train_label)
+    print 'load done'
     print dt
+    dt.build()
+    print 'train done'
+    # dt.traverse()
+
+    # test on train set
+    unit_test(dt, map(lambda y: y[0], dt.train_data), map(lambda y: y[1], dt.train_data))
+
+    # test on test set
+    test_data = load(f_test_data, f_test_label)
+    unit_test(dt, map(lambda y: y[0], test_data), map(lambda y: y[1], test_data))
